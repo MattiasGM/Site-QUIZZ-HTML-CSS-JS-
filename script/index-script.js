cd3e27317591612b1fdb036bacb2e7b7859e0ae0
Mattias Gaspar <mattiasmac2000@gmail.com>
2021-11-25 03:11:56 -0300

    css update

--- script/index-script.js
+++ script/index-script.js
@@ -1,5 +1,38 @@
 addEventListener('load', pageLoad)
 
 function pageLoad() {
+    let data = new Date(), hora = data.getHours()
+    let conteudo = document.querySelector('#conteudo')
+    let alternativas = document.querySelector('#ulRespotas')
+    let btnNext = document.querySelector('#btnNext')
     
+    backgroundInterative(hora)
+
+    let nome = initQuizz(conteudo, btnNext)
+
+}
+
+function backgroundInterative(h) {
+    if(h >= 8 && h < 16) {
+        main.style.background = '#d49253'
+        main.style.transition = '3s'
+    } else if(h >= 16 && h < 00) {
+        main.style.background = '#e2df1b'
+        main.style.transition = '3s'
+    } else {
+        main.style.background = '#11064b'
+        main.style.color = 'white'
+        main.style.transition = '3s'
+    }
+    return
+}
+
+function initQuizz(c,b) {
+    let nomeInput = document.createElement('input')
+    let btnNext = document.createElement('input')
+
+    btnNext.setAttribute('type', 'button')
+
+    c.appendChild(nomeInput)
+    c.appendChild(btnNext)
 }
