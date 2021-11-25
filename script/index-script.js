41917af30b0718520a35d67587cbf984da995465
Mattias Gaspar <mattiasmac2000@gmail.com>
2021-11-25 19:48:15 -0300

    update

--- script/index-script.js
+++ script/index-script.js
@@ -1,38 +1,45 @@
 addEventListener('load', pageLoad)
 
 function pageLoad() {
-    let data = new Date(), hora = data.getHours()
+    
     let conteudo = document.querySelector('#conteudo')
     let alternativas = document.querySelector('#ulRespotas')
-    let btnNext = document.querySelector('#btnNext')
+    let btnNext = document.querySelector('#divBtnNext')
     
-    backgroundInterative(hora)
+    
 
     let nome = initQuizz(conteudo, btnNext)
 
-}
-
-function backgroundInterative(h) {
-    if(h >= 8 && h < 16) {
-        main.style.background = '#d49253'
-        main.style.transition = '3s'
-    } else if(h >= 16 && h < 00) {
-        main.style.background = '#e2df1b'
-        main.style.transition = '3s'
-    } else {
-        main.style.background = '#11064b'
-        main.style.color = 'white'
-        main.style.transition = '3s'
-    }
-    return
 }
 
 function initQuizz(c,b) {
     let nomeInput = document.createElement('input')
     let btnNext = document.createElement('input')
 
+    nomeInput.setAttribute('id', 'boxName')
     btnNext.setAttribute('type', 'button')
+    btnNext.setAttribute('id', 'btnNext')
+    btnNext.setAttribute('value', 'Next')
 
     c.appendChild(nomeInput)
-    c.appendChild(btnNext)
+    b.appendChild(btnNext)
 }
+
+setInterval(
+    function () {
+        let data = new Date(), h = data.getHours()
+
+        if(h >= 8 && h < 16) {
+            main.style.background = '#d49253'
+            main.style.transition = '1s'
+        } else if(h >= 16 && h < 00) {
+            main.style.background = '#e2df1b'
+            main.style.transition = '1s'
+        } else {
+            main.style.background = '#11064b'
+            main.style.color = 'white'
+            main.style.transition = '1s'
+        }
+        
+        return
+    }, 300)
