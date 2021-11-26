0bcb59fb178b752821bcba5df7373ab34e815a94
Mattias Gaspar <mattiasmac2000@gmail.com>
2021-11-26 14:10:27 -0300

    update css

--- script/index-script.js
+++ script/index-script.js
@@ -16,6 +16,7 @@ function initQuizz(c,b) {
     let titulo = document.createElement('h1')
     let nomeInput = document.createElement('input')
     let btnNext = document.createElement('input')
+    let img = document.createElement('img')
 
     titulo.setAttribute('id', 'titulo')
     nomeInput.setAttribute('id', 'boxName')
@@ -23,29 +24,12 @@ function initQuizz(c,b) {
     btnNext.setAttribute('type', 'button')
     btnNext.setAttribute('id', 'btnNext')
     btnNext.setAttribute('value', 'Iniciar Quiz')
+    img.setAttribute('src', 'imagens/round-6.jpg')
 
-    titulo.innerHTML = 'Olá, Faça seu Quiz!'
+    titulo.innerHTML = 'Descubra quem você seria em Round 6!'
 
+    c.appendChild(img)
     c.appendChild(titulo)
     c.appendChild(nomeInput)
     b.appendChild(btnNext)
 }
-
-setInterval(
-    function () {
-        let data = new Date(), h = data.getHours()
-
-        if(h >= 8 && h < 16) {
-            main.style.background = '#d49253'
-            main.style.transition = '1s'
-        } else if(h >= 16 && h < 00) {
-            main.style.background = '#e2df1b'
-            main.style.transition = '1s'
-        } else {
-            main.style.background = '#11064b'
-            main.style.color = 'white'
-            main.style.transition = '1s'
-        }
-        
-        return
-    }, 300)
