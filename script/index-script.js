d29a0a1a08ab1437f9abe19c602cfe3d86c4f4a6
Mattias Gaspar <mattiasmac2000@gmail.com>
2021-11-29 19:43:49 -0300

    Update index-script.js

--- script/index-script.js
+++ script/index-script.js
@@ -10,8 +10,16 @@ let max = 10 // quantas perguntas o quiz vai possuir -- + de 10 ele pode ter 2 l
 let nome = '', contadorProgressBar = 0
 
 let perguntas = {
-        1: 'Você acha que ganharia quais jogos?',
-        2: ''
+        1: 'Você Acha Que Ganharia Quais Jogos?',
+        2: 'E Qual Você Morreria?',
+        3: 'Você Seria Capaz De Morrer Por Um Amigo',
+        4: 'Você faz de tudo pra cumprir com suas promessas?',
+        5: 'Você ama mais...',
+        6: 'Você é mais...',
+        7: 'Você se considera',
+        8: 'Você confia fácil nos outros?',
+        9: 'Qual você gosta mais?',
+        10: ''
     },
     alternativas = {
         1: {
@@ -24,7 +32,52 @@ let perguntas = {
             alternativa_7: 'Jogo da Lula'
         },
         2: {
-            
+            alternativa_1: 'Batatinha Frita 1, 2, 3',
+            alternativa_2: 'Colméia de Açucar',
+            alternativa_3: 'Briga a Noite',
+            alternativa_4: 'Cabo de Guerra',
+            alternativa_5: 'Bolinha de Gude',
+            alternativa_6: 'Ponte de Vidro',
+            alternativa_7: 'Jogo da Lula'
+        },
+        3: {
+            alternativa_1: 'Sim! Sou muito Fiel!',
+            alternativa_2: 'Não! Minha vida vale mais!',
+            alternativa_3: 'Depende quem...'
+        },
+        4: {
+            alternativa_1: 'Sim! Sempre!',
+            alternativa_2: 'Nem sempre...'
+        },
+        5: {
+            alternativa_1: 'Você mesmo',
+            alternativa_2: 'Os outros'
+        },
+        6: {
+            alternativa_1: 'Sentimental',
+            alternativa_2: 'Racional'
+        },
+        7: {
+            alternativa_1: 'Aberto para os outros e novas descobertas',
+            alternativa_2: 'Fechado e frio'
+        },
+        8: {
+            alternativa_1: 'Sim',
+            alternativa_2: 'Não',
+            alternativa_3: 'Depende'
+        },
+        9: {
+            alternativa_1: 'Gi-Hun',
+            alternativa_2: 'Ali',
+            alternativa_3: 'Sang-Woo',
+            alternativa_4: 'deok-su',
+            alternativa_5: 'Sae-Byeok',
+            alternativa_6: 'Ii-Nam',
+            alternativa_7: 'Ji Yeong',
+            alternativa_8: 'Mi Nyeo'
+        },
+        10: {
+
         }
     }
 
